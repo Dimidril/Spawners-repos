74c74c0a9149511928ae706b382555fb95989e96
Dmitry <Dimidril@users.noreply.github.com>
2020-12-18 17:17:16 +0300

    Выполнение
    Добавлен функционал по заданию 6

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Enemy _enemyForSpawn;
+
+    public void Spawn()
+    {
+        Enemy enemy = Instantiate(_enemyForSpawn, transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/SpawnersController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnersController : MonoBehaviour
+{
+    [SerializeField] private float _duration;
+
+    private Spawner[] _spawners;
+
+    private void Awake()
+    {
+        _spawners = gameObject.GetComponentsInChildren<Spawner>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(SpawnEnemyes());
+    }
+
+    private IEnumerator SpawnEnemyes()
+    {
+        var wait = new WaitForSeconds(_duration);
+        int spawnerIndex = 0;
+
+        while (true)
+        {
+            if(spawnerIndex >= _spawners.Length)
+                spawnerIndex = 0;
+            _spawners[spawnerIndex].Spawn();
+            spawnerIndex++;
+            yield return wait;
+        }
+    }
+}
